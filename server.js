f222f861dde22420e1d1fed54a3ea1717b4d5772
Matthew Laude <laude_matthew@yahoo.com>
2021-11-09 14:01:11 -0500

    backend is done

---
+++ server.js
@@ -0,0 +1,43 @@
+/////////////////////////////////
+// dependencies
+/////////////////////////////////
+const express = require("express")
+
+// Import JSON files
+const projects = require("./projects.json");
+const about = require("./about.json");
+
+
+/////////////////////////////////
+// Application Object
+/////////////////////////////////
+const app = express()
+
+/////////////////////////////////
+// MIDDLEWARE 
+/////////////////////////////////
+app.use(express.json()) // parse json bodies, and make req.body
+
+//home route for testing our app
+app.get("/", (req, res) => {
+  res.send("Hello World");
+});
+
+// route for retrieving projects
+app.get("/projects", (req, res) => {
+  // send projects via JSON
+  res.json(projects);
+});
+
+// route for retrieving about info
+app.get("/about", (req, res) => {
+  // send projects via JSON
+  res.json(about);
+});
+
+
+////////////////////////////////////
+//declare a variable for our port number
+const PORT = process.env.PORT || 4000;
+// turn on the server listener
+app.listen(PORT, () => console.log(`Listening on port ${PORT}`));
